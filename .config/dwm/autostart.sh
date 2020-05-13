dad4ab9b8f80fb4efe78d84640ac76beff8b4ea3
ShadowCurse <egik132@gmail.com>
2020-05-13 15:07:04 +0300

    update

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,11 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
-nitrogen --restore &
-xrandr --output DP-4 --mode 2560x1440 --rate 165 & xrandr --output HDMI-0  --right-of DP-4 & 
-
-setxkbmap -layout us,ru &
-setxkbmap -option 'grp:alt_shift_toggle' &
-
+nitrogen --restore 
+#xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
+setxkbmap -layout us,ru 
+setxkbmap -option 'grp:alt_shift_toggle' 
 while true; do
     xsetroot -name "$(date +"%A %B %d | %H:%M")"
     sleep 1m
--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -28,11 +28,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
-        { "QtCreator", NULL,      NULL,       1 << 1,       0,           -1 },
-        { "Google-chrome", NULL,   NULL,       1 << 4,       0,           -1 },
+	/* class           instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",          NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",       NULL,       NULL,       1 << 0,       0,           -1 },
+        { "QtCreator",     NULL,       NULL,       1 << 1,       0,           -1 },
+        { "Google-chrome", NULL,       NULL,       1 << 4,       0,           -1 },
+        { "clion",         NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
