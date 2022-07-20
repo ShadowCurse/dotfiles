cd785771e815f515beea460a53bb7521ed24701d
ShadowCurse <shadowcurse57@gmail.com>
2022-07-20 14:10:55 +0300

    nix config updates

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,43 +1,107 @@
-local M = {}
+local opt = vim.opt
+local g = vim.g
+local config = require("core.utils").load_config()
 
-local set = vim.opt
+vim.api.nvim_set_option('guifont', config.ui.guifont)
 
--- set.fileencoding = "utf-8" -- File content encoding for the buffer
--- set.spelllang = "en" -- Support US english
-set.clipboard = "unnamedplus" -- Connection to the system clipboard
-set.mouse = "a" -- Enable mouse support
--- set.signcolumn = "yes" -- Always show the sign column
--- set.foldmethod = "manual" -- Create folds manually
--- set.completeopt = { "menuone", "noselect" } -- Options for insert mode completion
--- set.colorcolumn = "99999" -- Fix for the indentline problem
--- set.backup = false -- Disable making a backup file
--- set.expandtab = true -- Enable the use of space in tab
--- set.hidden = true -- Ignore unsaved buffers
--- set.hlsearch = true -- Highlight all the matches of search pattern
-set.ignorecase = true -- Case insensitive searching
-set.smartcase = true -- Case sensitivie searching
--- set.spell = false -- Disable spelling checking and highlighting
--- set.showmode = false -- Disable showing modes in command line
--- set.smartindent = false -- Do auto indenting when starting a new line
--- set.splitbelow = true -- Splitting a new window below the current one
--- set.splitright = true -- Splitting a new window at the right of the current one
-set.swapfile = false -- Disable use of swapfile for the buffer
-set.termguicolors = true -- Enable 24-bit RGB color in the TUI
--- set.undofile = true -- Enable persistent undo
--- set.writebackup = false -- Disable making a backup before overwriting a file
-set.cursorline = true -- Highlight the text line of the cursor
-set.number = true -- Show numberline
--- set.relativenumber = false -- Show relative numberline
--- set.wrap = true -- Disable wrapping of lines longer than the width of window
--- set.conceallevel = 0 -- Show text normally
--- set.cmdheight = 1 -- Number of screen lines to use for the command line
--- set.shiftwidth = 2 -- Number of space inserted for indentation
-set.tabstop = 2 -- Number of space in a tab
-set.scrolloff = 8 -- Number of lines to keep above and below the cursor
--- set.sidescrolloff = 8 -- Number of columns to keep at the sides of the cursor
--- set.pumheight = 10 -- Height of the pop up menu
-set.history = 100 -- Number of commands to remember in a history table
--- set.timeoutlen = 300 -- Length of time to wait for a mapped sequence
--- set.updatetime = 300 -- Length of time to wait before triggering the plugin
+g.nvchad_theme = config.ui.theme
+g.toggle_theme_icon = "   "
+g.transparency = config.ui.transparency
+g.theme_switcher_loaded = false
 
-return M
+-- use filetype.lua instead of filetype.vim
+g.did_load_filetypes = 0
+g.do_filetype_lua = 1
+
+opt.laststatus = 3 -- global statusline
+opt.statusline = config.plugins.options.statusline.config
+opt.showmode = false
+
+opt.title = true
+opt.clipboard = "unnamedplus"
+opt.cul = true -- cursor line
+
+-- Indentline
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+
+-- disable tilde on end of buffer: https://github.com/neovim/neovim/pull/8546#issuecomment-643643758
+opt.fillchars = { eob = " " }
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop = 8
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+g.mapleader = " "
+
+-- disable some builtin vim plugins
+
+local default_plugins = {
+   "2html_plugin",
+   "getscript",
+   "getscriptPlugin",
+   "gzip",
+   "logipat",
+   "netrw",
+   "netrwPlugin",
+   "netrwSettings",
+   "netrwFileHandlers",
+   "matchit",
+   "tar",
+   "tarPlugin",
+   "rrhelper",
+   "spellfile_plugin",
+   "vimball",
+   "vimballPlugin",
+   "zip",
+   "zipPlugin",
+   "python3_provider",
+   "python_provider",
+   "node_provider",
+   "ruby_provider",
+   "perl_provider",
+   "tutor",
+   "rplugin",
+   "syntax",
+   "synmenu",
+   "optwin",
+   "compiler",
+   "bugreport",
+   "ftplugin",
+}
+
+for _, plugin in pairs(default_plugins) do
+   g["loaded_" .. plugin] = 1
+end
+
+-- set shada path
+vim.schedule(function()
+   vim.opt.shadafile = vim.fn.expand "$HOME" .. "/.local/share/nvim/shada/main.shada"
+   vim.cmd [[ silent! rsh ]]
+end)
+
+-- load user options if the file exists
+config.options.user()
--- .config/nvim/lua/core/utils.lua
+++ .config/nvim/lua/core/utils.lua
@@ -1,73 +1,164 @@
 local M = {}
+local api = vim.api
+local fn = vim.fn
 
-local g = vim.g
+local merge_tb = vim.tbl_deep_extend
 
-function M.bootstrap()
-  local fn = vim.fn
-  local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-      "git",
-      "clone",
-      "--depth",
-      "1",
-      "https://github.com/wbthomason/packer.nvim",
-      install_path,
-    }
-    print "Cloning packer...\nSetup NeoVim"
-    vim.cmd [[packadd packer.nvim]]
-  end
+M.close_buffer = function(force)
+   if vim.bo.buftype == "terminal" then
+      force = force or #api.nvim_list_wins() < 2 and ":bd!"
+      local swap = force and #api.nvim_list_bufs() > 1 and ":bp | bd!" .. fn.bufnr()
+      return vim.cmd(swap or force or "hide")
+   end
+
+   local fileExists = fn.filereadable(fn.expand "%p")
+   local modified = api.nvim_buf_get_option(fn.bufnr(), "modified")
+
+   -- if file doesnt exist & its modified
+   if fileExists == 0 and modified then
+      print "no file name? add it now!"
+      return
+   end
+
+   force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
+   -- if not force, change to prev buf and then close current
+   local close_cmd = force and ":bd!" or ":bp | bd" .. fn.bufnr()
+   vim.cmd(close_cmd)
 end
 
-function M.disabled_builtins()
-  g.loaded_gzip = false
-  g.loaded_netrwPlugin = false
-  g.loaded_netrwSettngs = false
-  g.loaded_netrwFileHandlers = false
-  g.loaded_tar = false
-  g.loaded_tarPlugin = false
-  g.zipPlugin = false
-  g.loaded_zipPlugin = false
-  g.loaded_2html_plugin = false
-  g.loaded_remote_plugins = false
+M.load_config = function()
+   local config = require "core.default_config"
+   local chadrc_exists, chadrc = pcall(require, "custom.chadrc")
+
+   if chadrc_exists then
+      -- merge user config if it exists and is a table; otherwise display an error
+      if type(chadrc) == "table" then
+         M.remove_default_keys()
+         config = merge_tb("force", config, chadrc)
+      else
+         error "chadrc must return a table!"
+      end
+   end
+
+   config.mappings.disabled = nil
+   return config
 end
 
--- function M.config()
---   return require "core.config"
--- end
+M.remove_default_keys = function()
+   local chadrc = require "custom.chadrc"
+   local user_mappings = chadrc.mappings or {}
+   local user_keys = {}
+   local user_sections = vim.tbl_keys(user_mappings)
 
-function M.compiled()
-  local compiled_ok, _ = pcall(require, "packer_compiled")
-  if compiled_ok then
-    require "packer_compiled"
-  end
+   -- push user_map keys in user_keys table
+   for _, section in ipairs(user_sections) do
+      user_keys = vim.tbl_deep_extend("force", user_keys, user_mappings[section])
+   end
+
+   local function disable_key(mode, keybind, mode_mapping)
+      local keys_in_mode = vim.tbl_keys(user_keys[mode] or {})
+
+      if vim.tbl_contains(keys_in_mode, keybind) then
+         mode_mapping[keybind] = nil
+      end
+   end
+
+   local default_mappings = require("core.default_config").mappings
+
+   -- remove user_maps from default mapping table
+   for _, section_mappings in pairs(default_mappings) do
+      for mode, mode_mapping in pairs(section_mappings) do
+         for keybind, _ in pairs(mode_mapping) do
+            disable_key(mode, keybind, mode_mapping)
+         end
+      end
+   end
 end
---
--- function M.list_registered_providers_names(filetype)
---   local s = require "null-ls.sources"
---   local available_sources = s.get_available(filetype)
---   local registered = {}
---   for _, source in ipairs(available_sources) do
---     for method in pairs(source.methods) do
---       registered[method] = registered[method] or {}
---       table.insert(registered[method], source.name)
---     end
---   end
---   return registered
--- end
---
--- function M.list_registered_formatters(filetype)
---   local null_ls_methods = require "null-ls.methods"
---   local formatter_method = null_ls_methods.internal["FORMATTING"]
---   local registered_providers = M.list_registered_providers_names(filetype)
---   return registered_providers[formatter_method] or {}
--- end
---
--- function M.list_registered_linters(filetype)
---   local null_ls_methods = require "null-ls.methods"
---   local formatter_method = null_ls_methods.internal["DIAGNOSTICS"]
---   local registered_providers = M.list_registered_providers_names(filetype)
---   return registered_providers[formatter_method] or {}
--- end
+
+M.load_mappings = function(mappings, mapping_opt)
+   -- set mapping function with/without whichkey
+   local map_func
+   local whichkey_exists, wk = pcall(require, "which-key")
+
+   if whichkey_exists then
+      map_func = function(keybind, mapping_info, opts)
+         wk.register({ [keybind] = mapping_info }, opts)
+      end
+   else
+      map_func = function(keybind, mapping_info, opts)
+         local mode = opts.mode
+         opts.mode = nil
+         vim.keymap.set(mode, keybind, mapping_info[1], opts)
+      end
+   end
+
+   mappings = mappings or vim.deepcopy(M.load_config().mappings)
+   mappings.lspconfig = nil
+
+   for _, section_mappings in pairs(mappings) do
+      -- skip mapping this as its mapppings are loaded in lspconfig
+      for mode, mode_mappings in pairs(section_mappings) do
+         for keybind, mapping_info in pairs(mode_mappings) do
+            -- merge default + user opts
+
+            local default_opts = merge_tb("force", { mode = mode }, mapping_opt or {})
+            local opts = merge_tb("force", default_opts, mapping_info.opts or {})
+
+            if mapping_info.opts then
+               mapping_info.opts = nil
+            end
+
+            map_func(keybind, mapping_info, opts)
+         end
+      end
+   end
+end
+
+-- remove plugins defined in chadrc
+M.remove_default_plugins = function(plugins)
+   local removals = M.load_config().plugins.remove or {}
+
+   if not vim.tbl_isempty(removals) then
+      for _, plugin in pairs(removals) do
+         plugins[plugin] = nil
+      end
+   end
+
+   return plugins
+end
+
+-- merge default/user plugin tables
+M.merge_plugins = function(default_plugins)
+   local user_plugins = M.load_config().plugins.user
+
+   -- merge default + user plugin table
+   default_plugins = merge_tb("force", default_plugins, user_plugins)
+
+   local final_table = {}
+
+   for key, _ in pairs(default_plugins) do
+      default_plugins[key][1] = key
+
+      final_table[#final_table + 1] = default_plugins[key]
+   end
+
+   return final_table
+end
+
+M.load_override = function(default_table, plugin_name)
+   local user_table = M.load_config().plugins.override[plugin_name]
+
+   if type(user_table) == "function" then
+      user_table = user_table()
+   end
+
+   if type(user_table) == "table" then
+      default_table = merge_tb("force", default_table, user_table)
+   else
+      default_table = default_table
+   end
+
+   return default_table
+end
 
 return M
