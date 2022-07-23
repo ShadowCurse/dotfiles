1f9edad0417c7f09f73e3e889c13bb95bc833bec
ShadowCurse <shadowcurse@noreply.codeberg.org>
2022-07-23 15:07:32 +0300

    dwm autostart update

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
+xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
 nitrogen --restore 
-#xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
-setxkbmap -layout us,ru 
-setxkbmap -option 'grp:alt_shift_toggle' 
+# setxkbmap -layout us,ru 
+# setxkbmap -option 'grp:alt_shift_toggle' 
 xinput --set-prop "SteelSeries SteelSeries Rival 600"  "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
-xset s 0 0 
-xset noblank
+# xset s 0 0 
+# xset noblank
 xset s off
-xset dpms 0 0 0 
+# xset dpms 0 0 0 
 xset -dpms
 while true; do
     xsetroot -name "$(date +"%A %B %d | %H:%M")"
