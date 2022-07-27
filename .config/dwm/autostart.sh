49283680619275212e1513aa92227aacafcf99fb
ShadowCurse <shadowcurse57@gmail.com>
2022-07-27 12:54:30 +0300

    dwm autostart update and fish with vi mode

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
+# xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
 nitrogen --restore 
-# setxkbmap -layout us,ru 
-# setxkbmap -option 'grp:alt_shift_toggle' 
 xinput --set-prop "SteelSeries SteelSeries Rival 600"  "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
-# xset s 0 0 
-# xset noblank
 xset s off
-# xset dpms 0 0 0 
 xset -dpms
 while true; do
     xsetroot -name "$(date +"%A %B %d | %H:%M")"
