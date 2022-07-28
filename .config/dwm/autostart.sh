3d2aa79f0657100147dd822150cbe7a8442278c2
ShadowCurse <shadowcurse@noreply.codeberg.org>
2022-07-28 15:05:14 +0300

    update to dwm autostart

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # xrandr --output DP-4 --mode 3440x1440 --rate 110 & xrandr --output HDMI-0  --right-of DP-4 & 
+xrandr --output DP-4 --mode 3440x1440 --rate 110 
 nitrogen --restore 
 xinput --set-prop "SteelSeries SteelSeries Rival 600"  "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 1
 xset s off
