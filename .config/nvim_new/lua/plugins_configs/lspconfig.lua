930ba7cb054e1c0cead7966da0e3e306e86f05df
ShadowCurse <shadowcurse57@gmail.com>
2022-08-04 19:41:41 +0300

    nvim config update

--- .config/nvim_new/lua/plugins_configs/lspconfig.lua
+++ .config/nvim_new/lua/plugins_configs/lspconfig.lua
@@ -146,3 +146,11 @@ lspconfig.rust_analyzer.setup {
     },
   },
 }
+
+local servers = { "zls", "rnix" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
