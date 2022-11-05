407b4c5e1702397604c1a17dbb0340e2ef55136e
ShadowCurse <shadowcurse@noreply.codeberg.org>
2022-11-05 16:18:58 +0300

    added trouble plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,6 +44,17 @@ local plugins = {
     module = "lspconfig",
   },
 
+  ["folke/trouble.nvim"] = {
+    -- requires = "kyazdani42/nvim-web-devicons",
+    -- config = function()
+    --   require("trouble").setup {
+    --     -- your configuration comes here
+    --     -- or leave it empty to use the default settings
+    --     -- refer to the configuration section below
+    --   }
+    -- end
+  },
+
   -- load luasnips + cmp related in insert mode only
 
   ["rafamadriz/friendly-snippets"] = {
