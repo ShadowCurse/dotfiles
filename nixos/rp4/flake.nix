00e2b6879871b12f6a6853938f3c8606f0efde44
ShadowCurse <shadowcurse57@gmail.com>
2023-07-21 19:24:48 +0100

    added nix configs for rasberry pi 4

---
+++ nixos/rp4/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+  };
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    images = {
+      pi = (self.nixosConfigurations.pi.extendModules {
+        modules = [ "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix" ];
+      }).config.system.build.sdImage;
+    };
+    nixosConfigurations = {
+      pi = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-hardware.nixosModules.raspberry-pi-4
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
+
