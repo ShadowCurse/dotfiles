0d2133c2c970704c96811a492daf44d37f9d53b7
ShadowCurse <shadowcurse@noreply.codeberg.org>
2023-09-12 17:36:59 +0100

    enabled treesitter, disabled it for severl languages

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,20 @@
 return {
-  -- "nvim-treesitter/nvim-treesitter",
-  --
-  -- cmd = { "TSInstall", "TSUninstall" },
-  -- build = ":TSUpdate",
-  --
-  -- config = function()
-  --   local treesitter = require("nvim-treesitter.configs")
-  --
-  --   local options = {
-  --     highlight = {
-  --       enable = true,
-  --       use_languagetree = true,
-  --     },
-  --   }
-  --
-  --   treesitter.setup(options)
-  -- end
+  "nvim-treesitter/nvim-treesitter",
+
+  cmd = { "TSInstall", "TSUninstall" },
+  build = ":TSUpdate",
+
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    local options = {
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+        disable = { "c", "query", "lua", "vim", "vimdoc" },
+      },
+    }
+
+    treesitter.setup(options)
+  end
 }
