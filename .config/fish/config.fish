a4dd501d54f053f04488ee079989ac391beae4d8
ShadowCurse <shadowcurse@noreply.codeberg.org>
2023-10-06 21:34:37 +0100

    replaced exa with eza + updated PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,22 +7,25 @@ function vim
 end
 
 function l
-  exa -lah --color=always --icons $argv
+  eza -lah --color=always --icons $argv
 end
 
 function ll
-  exa -lah --color=always --icons $argv
+  eza -lah --color=always --icons $argv
 end
+
+set -gx PATH $HOME/.local/bin $PATH
 
 set -x CARGO_INCREMENTAL 1
 set -x RUST_BACKTRACE 1
+set -gx PATH $HOME/.cargo/bin $PATH
 
 fish_vi_key_bindings
 set fish_greeting
 direnv hook fish | source
 starship init fish | source
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/antaraz/.ghcup/bin $PATH # ghcup-env
+# set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/antaraz/.ghcup/bin $PATH # ghcup-env
 
 if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
   fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
