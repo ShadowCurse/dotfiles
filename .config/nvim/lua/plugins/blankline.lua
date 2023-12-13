e467fc0b0ebcb6c1aaf9cdc3ee976f931be0d140
ShadowCurse <shadowcurse@noreply.codeberg.org>
2023-12-13 22:14:59 +0000

    updated blankline config

--- .config/nvim/lua/plugins/blankline.lua
+++ .config/nvim/lua/plugins/blankline.lua
@@ -1,28 +1,5 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-
-  config = function()
-    local blankline = require("indent_blankline")
-
-    local options = {
-      indentLine_enabled = 1,
-      char = "▏",
-      filetype_exclude = {
-        "help",
-        "terminal",
-        "alpha",
-        "packer",
-        "lspinfo",
-        "TelescopePrompt",
-        "TelescopeResults",
-        "lsp-installer",
-        "",
-      },
-      buftype_exclude = { "terminal" },
-      show_trailing_blankline_indent = false,
-      show_first_indent_level = false,
-    }
-
-    blankline.setup(options)
-  end
+  main = "ibl",
+  opts = {}
 }
