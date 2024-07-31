a2a10b7b0189d65ac2efe313abf1b3cc47456ebd
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-31 21:44:51 +0100

    update youtube script with ffmpeg processing

--- scripts/youtube_download.py
+++ scripts/youtube_download.py
@@ -1,6 +1,11 @@
 from pytube import YouTube
 import sys
+import subprocess
 
 for arg in sys.argv[1:]:
     print(arg)
-    YouTube(arg).streams.filter(only_audio=True).first().download()
+    print(f"Dowloading: {arg}")
+    path = YouTube(arg, use_oauth=True, allow_oauth_cache=True).streams.filter(only_audio=True).get_highest_resolution().download()
+    new_path = path.replace(".mp4", ".mp3")
+    print(f"Converting: {arg}")
+    subprocess.run(f"ffmpeg -i \"{path}\" \"{new_path}\"",shell=True,capture_output=True)
