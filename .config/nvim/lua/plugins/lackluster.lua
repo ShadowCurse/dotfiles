12fd1483dd7d1d88719ebcef687c692311f0af3b
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-08-02 17:59:40 +0100

    use tokyodark again

--- .config/nvim/lua/plugins/lackluster.lua
+++ .config/nvim/lua/plugins/lackluster.lua
@@ -3,6 +3,6 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("lackluster-mint")
+        -- vim.cmd.colorscheme("lackluster-mint")
     end,
 }
