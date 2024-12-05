acb4990c34b0fd4a81fc2d916058cd69dccf0c31
ShadowCurse <shadowcurse@noreply.codeberg.org>
2024-12-05 00:54:06 +0000

    make nvim-tree into floating window

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -22,6 +22,32 @@ return {
         highlight_git = true,
         highlight_opened_files = "none",
       },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local width_p = 0.8
+            local height_p = 0.8
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local col = (screen_w * (1.0 - width_p)) / 2.0
+            local row = (screen_h * (1.0 - height_p)) / 2.0
+            local width = screen_w * width_p
+            local height = screen_h * height_p
+            return {
+              border = "rounded",
+              relative = "editor",
+              col = math.floor(col),
+              row = math.floor(row),
+              width = math.floor(width),
+              height = math.floor(height),
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * 5)
+        end,
+      },
     }
 
     nvimtree.setup(options)
